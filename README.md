2e6a3853da4a5d5d603d847d5b21bd965c026be7
Michael Kaplan <mkaplanmnj@yahoo.com>
2025-02-21 12:10:11 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # Email Reader
+
 Email reader for reading emails from yahoo account
-
-https://gist.github.com/martinrusev/6121028
-
-M.search(None, '(SINCE "01-Jan-2012")')
-M.search(None, '(BEFORE "01-Jan-2012")')
-M.search(None, '(SINCE "01-Jan-2012" BEFORE "02-Jan-2012")')
--- read_email.py
+++ read_email.py
@@ -4,6 +4,8 @@ from datetime import datetime, timezone
 from typing import Dict
 from pathlib import Path
 import os
+from schedule import every, repeat, run_pending
+import time
 
 
 supported_types = [
@@ -15,8 +17,16 @@ supported_types = [
     "application/rtf",
 ]
 
+cwd = os.getcwd()
+
 
 def utc_to_local(utc_dt: datetime) -> datetime:
+    """
+    Convert date/time to local time zone
+
+    Args:
+
+    """
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
 
 
@@ -32,7 +42,7 @@ def read_json_secret_file(file_path: str) -> (Dict | None):
         or None if an error occurs.
     """
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             data: Dict = json.load(file)
             return data
     except FileNotFoundError:
@@ -58,10 +68,9 @@ def get_last_finish_time(
     Returns:
     tuple: date time
     """
-    cwd = os.getcwd()
     date_file_path = Path(os.path.join(cwd, date_file))
     if date_file_path.is_file():
-        with open(date_file_path, 'r') as file:
+        with open(date_file_path, "r") as file:
             date: str = file.read()
             date_time: datetime = datetime.strptime(
                 date, "%Y-%m-%d %H:%M:%S %z")
@@ -70,10 +79,10 @@ def get_last_finish_time(
             file.write(start_date)
             date_time: datetime = datetime.strptime(
                 start_date, "%Y-%m-%d %H:%M:%S %z")
-    date_time = utc_to_local(date_time)
+    last_date_time = utc_to_local(date_time)
     last_date: datetime = date_time.date()
     last_time: datetime = date_time.time()
-    return (last_date, last_time)
+    return (last_date_time, last_date, last_time)
 
 
 def set_last_finish_time(date_file: str, start_date: datetime) -> None:
@@ -84,50 +93,61 @@ def set_last_finish_time(date_file: str, start_date: datetime) -> None:
     date_file: path to the file
     start_date: datetime last scan
     """
-    cwd = os.getcwd()
+    start_date = utc_to_local(start_date)
     date_file_path = Path(os.path.join(cwd, date_file))
     start_date_str: str = start_date.strftime("%Y-%m-%d %H:%M:%S %z")
-    with open(date_file_path, 'w') as file:
+    with open(date_file_path, "w") as file:
         file.write(start_date_str)
 
 
-date_string_z = "2025-03-01 10:30:00 +0800"
-parsed_dt_z = datetime.strptime(date_string_z, "%Y-%m-%d %H:%M:%S %z")
-print(parsed_dt_z)
+@repeat(every(1).hour)
+def extract_attachments_from_mailbox():
+    """
+    Reads emails from mailbox and sends qualified attachments
+    fo document store
+    """
+    secrets: Dict = read_json_secret_file("secrets.json")
+    username = secrets.get("username")
+    password = secrets.get("password")
+    initial_folder = secrets.get("initial_folder")
+    imap_server = secrets.get("imap_server")
+    date_file = secrets.get("date_file")
+    start_date = secrets.get("start_date")
 
+    last_date_time, last_date, _ = get_last_finish_time(
+        date_file, start_date)
 
-secrets: Dict = read_json_secret_file("secrets.json")
-username = secrets.get('username')
-password = secrets.get('password')
-imap_server = secrets.get("imap_server")
-date_file = secrets.get("date_file")
-start_date = secrets.get("start_date")
+    attachments_file_path = Path(os.path.join(cwd, "attachments"))
 
-last_date, last_time = get_last_finish_time(date_file, start_date)
-
-cwd = os.getcwd()
-attachments_file_path = Path(os.path.join(cwd, "attachments"))
-
-with MailBox(imap_server).login(
-        username, password) as mailbox:
-    for msg in mailbox.fetch(AND(date_gte=last_date)):
-
-        print(utc_to_local(msg.date).time())
-        print(last_time)
-
-        for att in msg.attachments:
-            try:
-                if att.filename == '':
-                    continue
-                if att.content_type not in supported_types:
-                    continue
-                with open(
-                    f'{attachments_file_path}/{msg.from_} {att.filename}', 'wb'
-                ) as f:
-                    print(att.filename, att.content_type)
-                    f.write(att.payload)
-            except Exception as e:
-                print(e, att.filename, att.content_type)
+    with MailBox(imap_server).login(
+            username=username,
+            password=password,
+            initial_folder=initial_folder) as mailbox:
+        for msg in mailbox.fetch(criteria=AND(date_gte=last_date)):
+            adjust_msg_date = utc_to_local(msg.date)
+            if adjust_msg_date < last_date_time:
                 continue
+            for att in msg.attachments:
+                try:
+                    if att.filename == "":
+                        continue
+                    if att.content_type not in supported_types:
+                        continue
+                    # send file to document store
+                    with open(
+                        f"{attachments_file_path}/{
+                            msg.from_} {att.filename}", "wb"
+                    ) as f:
+                        print(att.filename, att.content_type)
+                        f.write(att.payload)
+                except Exception as e:
+                    print(e, att.filename, att.content_type)
+                    continue
 
-set_last_finish_time(date_file, datetime.now())
+    set_last_finish_time(date_file, datetime.now())
+
+
+if __name__ == "__main__":
+    while True:
+        run_pending()
+        time.sleep(1)
